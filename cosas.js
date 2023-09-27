22c3ce0d06d3f0576bb010ab797b0b64cc47bd6a
Ivan PM <cdmx2125@amerike.edu.mx>
2023-09-27 13:13:09 -0600

    Listo jijija
    Jijijia

--- cosas.js
+++ cosas.js
@@ -1,13 +1,85 @@
 
 console.log("Probando probando xd");
 
-let nombre = "Ivan",
-    ApellidoP= "Paniagua",
-    ApellidoM="Maldonado";
+let datosPersonales={
+    nombre : "Ivan Paniagua Maldonado",
+    Edad: 20,
+    soyMainRidley:true,
+    pasatiempos:["Jugar Videojuegos","Ver vídeos","Jugar/Pasear a mi perro","Leer"],
+    contacto:{
+        email:"ipaniaguamaldonado@gmail.com",
+        instagram:"panvan23"
+    },
+    obtenerEdad: function()
+    {
+        return this.Edad;
+    },    
 
-let edad=20,
-    gmail="cdmx2125@amerike.edu.mx";
+    LibrosArray:[
+        {
+            nombreLibro:"El Rey Carbón",
+            primerNombreAutor:"Sinclair",
+            primerApellidoAutor:"Upton",
+            País:"México",
+            Publisher:"Fondo de Cultura Económica",
+            formatoAppa: function()
+            {
+                console.log(`Autor: ${this.primerNombreAutor}`);
+                console.log(`Apellido: ${this.primerApellidoAutor}`);
+                console.log(`Nombre_del_libro: ${this.nombreLibro}`);
+                console.log(`País: ${this.País}`);
+                console.log(`Publisher: ${this.Publisher}`);                
+            }
+        },
+        {
+            nombreLibro:"Ask Iwata",
+            primernombreEditor:" Hobonichi",
+            primerApellidoAutor:" ",
+            País:"Estados Unidos",
+            Publisher:"VIZ Media LLC",
+            formatoAppa: function()
+            {
+                console.log(`Editor: ${this.primernombreEditor}`);
+                console.log(`Apellido: ${this.primerApellidoAutor}`);
+                console.log(`Nombre_del_libro: ${this.nombreLibro}`);
+                console.log(`País: ${this.País}`);
+                console.log(`Publisher: ${this.Publisher}`);                
+            }
+        },
+        {
+            nombreLibro:"El Juego de Ender",
+            primerNombreAutor:"Orson",
+            primerApellidoAutor:"Scott",
+            País:"España",
+            Publisher:"PRH Grupo Editorial",
+            formatoAppa: function()
+            {
+                console.log(`Autor: ${this.primerNombreAutor}`);
+                console.log(`Apellido: ${this.primerApellidoAutor}`);
+                console.log(`Nombre_del_libro: ${this.nombreLibro}`);
+                console.log(`País: ${this.País}`);
+                console.log(`Publisher: ${this.Publisher}`);                
+            }
+        },
+    ],
 
-let mensajeshido=  `Hola qué tal como te va?, mi nombre es ${nombre} ${ApellidoP} ${ApellidoM}, y tengo ${edad} años. Mi correo es ${gmail}`;
+};
 
-let pasatiemposhidos= 
+console.log("DatosPersonales: ");
+console.log(`Nombre: ${datosPersonales.nombre}`);
+console.log(`Edad: ${datosPersonales.nombre}`);
+console.log(`Email: ${datosPersonales.nombre}`);
+console.log(`Es main Ridley? ${datosPersonales.soyMainRidley}`);
+console.log(`Teléfono: ${datosPersonales.nombre}`);
+console.log(`Pasatiempos: ${datosPersonales.nombre}`);
+
+console.log("\nLibros Favoritos:");
+console.log("\n");
+
+
+
+datosPersonales.LibrosArray.forEach(libro => {
+    libro.formatoAppa();
+    console.log("");
+});
+
